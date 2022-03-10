6a9c72f337af8caa40cf2d69805a39e7bbc26f02
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-03-10 09:47:30 +0100

    fix missing ;

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -19,7 +19,7 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
-    include_file('desktop', '404', 'php')
+    include_file('desktop', '404', 'php');
     die();
 }
 ?>
