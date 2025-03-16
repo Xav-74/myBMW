860cc6dcd06ea62188b845415971f1359f9c1cbd
Xav-74 <56156622+Xav-74@users.noreply.github.com>
2025-03-16 18:14:39 +0100

    Check cronPattern

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -37,7 +37,7 @@ if (!isConnect()) {
         </label>
         <div class="col-sm-3">
 		    <div class="input-group">
-                <input id="cronPattern" class="form-control configKey" data-l1key="cronPattern" placeholder="*/30 * * * *" readonly/>
+                <input id="cronPattern" class="form-control configKey" data-l1key="cronPattern" placeholder="*/30 * * * *"/>
                 <span class="input-group-btn">
                     <a class="btn btn-primary jeeHelper" data-helper="cron" title="{{Assistant cron}}"><i class="fas fa-question-circle"></i></a>
                 </span>
@@ -62,28 +62,33 @@ if (!isConnect()) {
     function scheduleCron()  {
         
         var cronPattern = document.getElementById('cronPattern').value;
-        $.ajax({
-            type: "POST",
-            url: "plugins/myBMW/core/ajax/myBMW.ajax.php",
-            data: {
-                action: "scheduleCron",
-                cronPattern: cronPattern,
-                },
-            dataType: 'json',
-                error: function (request, status, error) {
-                handleAjaxError(request, status, error);
-                },
-            success: function (data) { 			
+        const cronRegex = /(^((\*\/)?([0-5]?[0-9])((\,|\-|\/)([0-5]?[0-9]))*|\*) ((\*\/)?((2[0-3]|1[0-9]|[0-9]|00))((\,|\-|\/)(2[0-3]|1[0-9]|[0-9]|00))*|\*) ((\*\/)?([1-9]|[12][0-9]|3[01])((\,|\-|\/)([1-9]|[12][0-9]|3[01]))*|\*) ((\*\/)?([1-9]|1[0-2])((\,|\-|\/)([1-9]|1[0-2]))*|\*|(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|des)) ((\*\/)?[0-6]((\,|\-|\/)[0-6])*|\*|00|(sun|mon|tue|wed|thu|fri|sat))\s*$)|@(annually|yearly|monthly|weekly|daily|hourly|reboot)/; 
+        
+        if ( cronRegex.test(cronPattern) == true ) {
+            $.ajax({
+                type: "POST",
+                url: "plugins/myBMW/core/ajax/myBMW.ajax.php",
+                data: {
+                    action: "scheduleCron",
+                    cronPattern: cronPattern,
+                    },
+                dataType: 'json',
+                    error: function (request, status, error) {
+                    handleAjaxError(request, status, error);
+                    },
+                success: function (data) { 			
 
-                if (data.state != 'ok') {
-                    $('#div_alert').showAlert({message: '{{Erreur lors de la mise à jour du cron}}'+' ('+cronPattern+')', level: 'danger'});
-                    return;
+                    if (data.state != 'ok') {
+                        $('#div_alert').showAlert({message: '{{Erreur lors de la mise à jour du cron}}'+' ('+cronPattern+')', level: 'danger'});
+                        return;
+                    }
+                    else  {
+                        $('#div_alert').showAlert({message: '{{Mise à jour du cron réalisée avec succès}}'+' ('+cronPattern+')', level: 'success'});
+                    }
                 }
-                else  {
-                    $('#div_alert').showAlert({message: '{{Mise à jour du cron réalisée avec succès}}'+' ('+cronPattern+')', level: 'success'});
-                }
-            }
-        });
+            });
+        }
+        else { $('#div_alert').showAlert({message: '{{Expression cron erronée}}', level: 'danger'}); }
     };
 
 </script>
